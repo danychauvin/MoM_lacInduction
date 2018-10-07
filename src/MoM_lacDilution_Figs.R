4d105090f5ecaee2010a05f4802e020f78a76e4e
Thomas Julou <thomas.julou@gmail.com>
2018-10-07 15:01:55 +0200

    improved Fig.2

--- src/MoM_lacDilution_Figs.R
+++ src/MoM_lacDilution_Figs.R
@@ -28,16 +28,48 @@ save_plot(here("plots", "MoM_lacDilution_fig1.pdf"), fig1,
 )
 
 #####
-fig2 <- 
+(fig2 <- (function(.relh=c(1.4, 1.1), .relw=c(4.5, 3), .prop_breaks=c(0, 0.5, 1))
   plot_grid(
-    myplots[['basal_perturb_pre']],
-    myplots[['glyc_frac_short']] ,
-    labels="AUTO", nrow=1, rel_widths=c(2, 1), align='h'
-  )
+    # first row
+    plot_grid(
+      get_legend(myplots[['basal_perturb_pre_violin']] +
+                   theme(legend.justification = c(0.5, 0))),
+      NULL,
+      labels="AUTO", nrow=1, rel_widths=c(.relw)),
+    # second row
+    plot_grid(
+      plot_grid(
+        myplots[['basal_perturb_pre_violin']] +
+          theme(axis.text.x=element_blank(),
+                legend.position = 'none',
+                # legend.position = c(1, 1), legend.justification = c(1, 1),
+          ),
+        myplots[['basal_perturb_pre_frac_short']] +
+          scale_y_continuous(breaks=.prop_breaks) +
+          labs(y='prop. of\nshort lags') +
+          guides(fill='none') +
+          NULL,
+        ncol=1, rel_heights = .relh, align='v'),
+      plot_grid(
+        # NULL,
+        myplots[['glyc_mix_violin']] +
+          theme(axis.text.x=element_blank(),
+                axis.title.y=element_blank(),
+                legend.position = 'none'
+          ),
+        myplots[['glyc_mix_frac_short']] +
+          scale_y_continuous(breaks=.prop_breaks) +
+          guides(fill='none') +
+          theme(axis.title.y=element_blank()) +
+          NULL,
+        ncol=1, rel_heights = .relh, align='v'),
+    nrow=1, rel_widths=.relw, align='h'),
+    ncol=1, rel_heights=c(0.15, sum(.relh))
+  ))())
 
 save_plot(here("plots", "MoM_lacDilution_fig2.pdf"), fig2,
           base_height=NULL, base_width=4.75 * 14/8, # 2 cols
-          base_aspect_ratio = 2.2
+          base_aspect_ratio = 1.6
 )
 
 #####
